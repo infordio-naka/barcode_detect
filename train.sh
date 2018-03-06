d665da9279c33acfbf21554ab569474f7b8e1a6a
naka <naka@example.com>
2018-03-06 10:29:07 +0900

    fix train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-# python gen_barcode/gen_barcode.py
-opencv_createsamples -info positive.dat -vec positive.vec -num 7000 -w 40 -h 40
-opencv_traincascade -data cascade/ -vec positive.vec -bg negative.dat -numPos 7000 -numNeg 5000 -w 40 -h 40
+# rm -fr images/*; python gen_barcode/gen_barcode.py
+# opencv_createsamples -info positive.dat -vec positive.vec -num 30000 -w 45 -h 45
+rm -fr cascade/*; opencv_traincascade -data cascade -vec positive.vec -bg negative.dat -numStages 10 -numPos 25000 -numNeg 20000 -w 45 -h 45 -maxFalseAlarmRate 0.001 -minHitRate 0.3 -featureType LBP
